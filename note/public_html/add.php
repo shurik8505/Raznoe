6a6ed520e0c91597dd573f14b57dae88474d579c
Aleksandr <shurik8505@gmail.com>
2020-11-30 21:54:54 +0200

    Simply Notes

---
+++ note/public_html/add.php
@@ -0,0 +1,17 @@
+<?php
+  $task = $_POST['task'];
+  if($task == '') {
+    echo 'Введите заметку';
+    exit();
+  }
+
+  require 'configDB.php';
+
+  $sql = 'INSERT INTO tasks(task) VALUES(:task)';
+  $query = $pdo->prepare($sql);
+  $query->execute(['task' => $task]);
+
+  header('Location: /');
+
+
+?>
---
+++ note/public_html/configDB.php
@@ -0,0 +1,4 @@
+<?php
+  $dsn = 'mysql:host=localhost;dbname=notes';
+  $pdo = new PDO($dsn, 'shurik', '1');
+?>
